fa1b29a74ac1e17889688152724df9b2a8a7451b
Nanda <nanda4world@gmail.com>
2026-01-14 00:36:02 +0530

    fix of vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚠️ IMPORTANT: repo name must match exactly
 export default defineConfig({
+  base: '/storyworld/',
   plugins: [react()],
 })
